22c0bca25a574cd06d6304171872d7768c8a5955
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-22 08:11:13 +0900

    add zod validation to LoginForm.tsx 19

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -3,15 +3,20 @@
 import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react';
 import { GiPadlock } from 'react-icons/gi';
 import { useForm } from 'react-hook-form';
+import { loginSchema, LoginSchema } from '@/lib/schemas/loginSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const LoginForm = () => {
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm();
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onTouched',
+  });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: LoginSchema) => {
     console.log(data);
   };
 
@@ -33,7 +38,7 @@ const LoginForm = () => {
               defaultValue={''}
               label={'Email'}
               variant={'bordered'}
-              {...register('email', { required: 'Email is required' })}
+              {...register('email')}
               isInvalid={!!errors.email}
               errorMessage={errors.email?.message as string}
             />
@@ -42,7 +47,8 @@ const LoginForm = () => {
               label={'Password'}
               variant={'bordered'}
               type={'password'}
-              {...register('password', { required: 'Password is required' })}
+              {...register('password')}
+              isInvalid={!!errors.password}
               errorMessage={errors.password?.message as string}
             />
             <Button isDisabled={!isValid} fullWidth color={'secondary'} type={'submit'}>
