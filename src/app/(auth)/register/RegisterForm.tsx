3e0503fe6ee280121cc9dc76e6048c8d7e1a486e
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-23 10:36:23 +0900

    finish 28(Handling errors in the form Part 2)

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -11,7 +11,8 @@ const RegisterForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    setError,
+    formState: { errors, isValid, isSubmitting },
   } = useForm<RegisterSchema>({
     // resolver: zodResolver(registerSchema),
     mode: 'onTouched',
@@ -19,7 +20,19 @@ const RegisterForm = () => {
 
   const onSubmit = async (data: RegisterSchema) => {
     const result = await registerUser(data);
-    console.log(result);
+
+    if (result.status === 'success') {
+      console.log('User registered successfully');
+    } else {
+      if (Array.isArray(result.error)) {
+        result.error.forEach((e) => {
+          const fieldName = e.path.join('.') as 'email' | 'name' | 'password';
+          setError(fieldName, { message: e.message });
+        });
+      } else {
+        setError('root.serverError', { message: result.error });
+      }
+    }
   };
 
   return (
@@ -61,7 +74,8 @@ const RegisterForm = () => {
               isInvalid={!!errors.password}
               errorMessage={errors.password?.message as string}
             />
-            <Button isDisabled={!isValid} fullWidth color={'secondary'} type={'submit'}>
+            {errors.root?.serverError && <p className={'text-danger text-sm'}>{errors.root.serverError.message}</p>}
+            <Button isLoading={isSubmitting} isDisabled={!isValid} fullWidth color={'secondary'} type={'submit'}>
               Register
             </Button>
           </div>
