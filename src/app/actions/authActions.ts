c37dabc5d41dcbc061adb2d32b97f2d1c0af285c
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-23 16:25:47 +0900

    finish 29(Signing in users Part 1)

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -34,3 +34,11 @@ export async function registerUser(data: RegisterSchema): Promise<ActionResult<U
     return { status: 'error', error: 'Something went wrong' };
   }
 }
+
+export async function getUserByEmail(email: string) {
+  return prisma.user.findUnique({ where: { email } });
+}
+
+export async function getUserById(id: string) {
+  return prisma.user.findUnique({ where: { id } });
+}
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,29 @@
-import GitHub from 'next-auth/providers/github';
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from '@auth/core/providers/credentials';
+import { loginSchema } from '@/lib/schemas/loginSchema';
+import { getUserByEmail } from '@/app/actions/authActions';
+import { compare } from 'bcryptjs';
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+export default {
+  providers: [
+    Credentials({
+      name: 'credentials',
+      async authorize(creds) {
+        const validated = loginSchema.safeParse(creds);
+
+        if (validated.success) {
+          // extract email and password
+          const { email, password } = validated.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !(await compare(password, user.passwordHash))) return null;
+
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
