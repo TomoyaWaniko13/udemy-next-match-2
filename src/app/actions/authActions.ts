fb73f0d79ffd11f5e61adedb8cb8c1650ee3b3c7
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-23 09:03:00 +0900

    finish 27(Handling errors in the form)

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -3,25 +3,34 @@
 import { registerSchema, RegisterSchema } from '@/lib/schemas/registerSchema';
 import bcrypt from 'bcryptjs';
 import { prisma } from '@/lib/prisma';
+import { User } from '@prisma/client';
+import { ActionResult } from '@/types';
 
-export async function registerUser(data: RegisterSchema) {
-  const validated = registerSchema.safeParse(data);
+export async function registerUser(data: RegisterSchema): Promise<ActionResult<User>> {
+  try {
+    const validated = registerSchema.safeParse(data);
 
-  if (!validated.success) {
-    return { error: validated.error.errors };
+    if (!validated.success) {
+      return { status: 'error', error: validated.error.errors };
+    }
+
+    const { name, email, password } = validated.data;
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) return { status: 'error', error: 'User already exists' };
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: { name, email, passwordHash: hashedPassword },
+    });
+
+    return { status: 'success', data: user };
+  } catch (error) {
+    console.log(error);
+    return { status: 'error', error: 'Something went wrong' };
   }
-
-  const { name, email, password } = validated.data;
-
-  const existingUser = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (existingUser) return { error: 'User already exists' };
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  return prisma.user.create({
-    data: { name, email, passwordHash: hashedPassword },
-  });
 }
