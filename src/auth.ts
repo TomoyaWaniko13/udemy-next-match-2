2d849237059a262a6de8695cde82e1cf07864eba
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-24 11:00:41 +0900

    finish 33(Using NextAuth callbacks)

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,18 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import authConfig from './auth.config';
 import { prisma } from '@/lib/prisma';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,
