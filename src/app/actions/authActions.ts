3e7e4f29075fc0bcc8480a732ae817661b140331
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-28 09:48:42 +0900

    finish 54(Adding the like toggle function)

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -6,7 +6,7 @@ import { prisma } from '@/lib/prisma';
 import { User } from '@prisma/client';
 import { ActionResult } from '@/types';
 import { LoginSchema } from '@/lib/schemas/loginSchema';
-import { signIn, signOut } from '@/auth';
+import { auth, signIn, signOut } from '@/auth';
 import { AuthError } from 'next-auth';
 
 export async function signInUser(data: LoginSchema): Promise<ActionResult<string>> {
@@ -76,4 +76,13 @@ export async function getUserByEmail(email: string) {
 
 export async function getUserById(id: string) {
   return prisma.user.findUnique({ where: { id } });
+}
+
+export async function getAuthUserId() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error('Unauthorized');
+
+  return userId;
 }
---
+++ src/app/actions/likeActions.ts
@@ -0,0 +1,37 @@
+'use server';
+
+import { auth } from '@/auth';
+import { prisma } from '@/lib/prisma';
+import { getAuthUserId } from '@/app/actions/authActions';
+
+export async function toggleLikeMember(targetUserId: string, isLiked: boolean) {
+  try {
+    const userId = await getAuthUserId();
+
+    if (isLiked) {
+      await prisma.like.delete({
+        where: { sourceUserId_targetUserId: { sourceUserId: userId, targetUserId } },
+      });
+    } else {
+      await prisma.like.create({
+        data: { sourceUserId: userId, targetUserId },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function fetchCurrentUserLikeIds() {
+  try {
+    const userId = await getAuthUserId();
+
+    const likeIds = await prisma.like.findMany({
+      where: { sourceUserId: userId },
+      select: { targetUserId: true },
+    });
+
+    return likeIds.map((like) => like.targetUserId);
+  } catch (error) {}
+}
