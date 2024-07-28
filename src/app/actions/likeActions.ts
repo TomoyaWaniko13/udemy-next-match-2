17f528b73c5c4112e38c7e663b9f10498d26bf9a
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-28 16:27:41 +0900

    finish 57(Adding the list actions)

--- src/app/actions/likeActions.ts
+++ src/app/actions/likeActions.ts
@@ -36,3 +36,59 @@ export async function fetchCurrentUserLikeIds() {
     throw error;
   }
 }
+
+export async function fetchLikedMembers(type = 'source') {
+  try {
+    const userId = await getAuthUserId();
+
+    switch (type) {
+      case 'source':
+        return await fetchSourceLikes(userId);
+      case 'target':
+        return await fetchTargetLikes(userId);
+      case 'mutual':
+        return await fetchMutualLikes(userId);
+      default:
+        return [];
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+const fetchSourceLikes = async (userId: string) => {
+  const sourceList = await prisma.like.findMany({
+    where: { sourceUserId: userId },
+    select: { targetMember: true },
+  });
+
+  return sourceList.map((x) => x.targetMember);
+};
+
+const fetchTargetLikes = async (userId: string) => {
+  const targetList = await prisma.like.findMany({
+    where: { targetUserId: userId },
+    select: { sourceMember: true },
+  });
+
+  return targetList.map((x) => x.sourceMember);
+};
+
+const fetchMutualLikes = async (userId: string) => {
+  const likedUsers = await prisma.like.findMany({
+    where: { sourceUserId: userId },
+    select: { targetUserId: true },
+  });
+
+  const likeIds = likedUsers.map((x) => x.targetUserId);
+
+  const mutualList = await prisma.like.findMany({
+    where: {
+      AND: [{ targetUserId: userId }, { sourceUserId: { in: likeIds } }],
+    },
+    select: { sourceMember: true },
+  });
+
+  return mutualList.map((x) => x.sourceMember);
+};
